50d0dc1fa5e2a816d8a669e18581a8ad694dc43a
shsh159 <sunghoon8467@gmail.com>
2025-03-21 11:30:14 +0900

    입금, 출금 폼 DatePicer 추가

--- src/app/expense/page.tsx
+++ src/app/expense/page.tsx
@@ -4,11 +4,18 @@ import { useState } from "react";
 import { TextField, Button, Typography, Box, Modal, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import styles from "./page.module.scss";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
+import "dayjs/locale/ko"; // 한글 로케일 추가
 
 interface AddExpenseModalProps {
   open: boolean;
   handleClose: () => void;
 }
+
+const today = dayjs();
 
 export default function AddExpenseModal({ open, handleClose }: AddExpenseModalProps) {
   const [amount, setAmount] = useState("");
@@ -50,6 +57,12 @@ export default function AddExpenseModal({ open, handleClose }: AddExpenseModalPr
             onChange={(e) => setDescription(e.target.value)}
             required
           />
+          <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
+            <DatePicker
+              defaultValue={today}
+              views={['year', 'month', 'day']}
+            />
+          </LocalizationProvider>
           <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
             추가하기
           </Button>
--- src/app/income/page.tsx
+++ src/app/income/page.tsx
@@ -4,11 +4,18 @@ import { useState } from "react";
 import { TextField, Button, Typography, Box, Modal, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import styles from "./page.module.scss";
+import dayjs from "dayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import "dayjs/locale/ko"; // 한글 로케일 추가
 
 interface AddIncomeModalProps {
   open: boolean;
   handleClose: () => void;
 }
+
+const today = dayjs();
 
 export default function AddIncomeModal({ open, handleClose }: AddIncomeModalProps) {
   const [amount, setAmount] = useState("");
@@ -50,6 +57,12 @@ export default function AddIncomeModal({ open, handleClose }: AddIncomeModalProp
             onChange={(e) => setDescription(e.target.value)}
             required
           />
+          <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
+            <DatePicker
+              defaultValue={today}
+              views={['year', 'month', 'day']}
+            />
+          </LocalizationProvider>
           <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
             추가하기
           </Button>
