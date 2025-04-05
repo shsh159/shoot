0941b9223a499c11c9470b898688a886d35d0a93
shsh159 <sunghoon8467@gmail.com>
2025-04-05 14:06:07 +0900

    express 연동 확인 완료

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 4000;
 
-app.get('/api', (req, res) => {
-    console.log('test')
-    res.send('Hello World')
-})
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000', // 허용할 출처
+}));
 
-app.listen(port, ()=> {
-    console.log(`app listening on port${port}`)
-})
+const users = {
+    id: 1,
+    member: [  // 'member'로 수정
+        { id: 1, name: 'John Doe' },
+        { id: 2, name: 'Jane Doe' }
+    ]
+};
+
+app.get('/list', (req, res) => {
+    console.log('Fetching list');
+    res.json(users);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,29 +1,54 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Button } from "@mui/material";
 import HistoryGrid from "../components/grid/historyGrid";
 import DefaultLayout from "../components/layout/defaultLayout";
 import styles from "./page.module.scss";
 import HistoryAddModal from "../components/modal/historyAddModal";
+import axios from "axios";
+
+interface Typee {
+  id: number;
+  member: Array<{ id: number; name: string }>;
+}
 
 export default function List() {
   const [modalOpen, setModalOpen] = useState(false);
+  const [data, setData] = useState<Typee | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/list');
+        
+        // 응답 데이터를 그대로 상태에 저장
+        setData(response.data); 
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <DefaultLayout>
-        <Box className={styles.contentWrapper}>
-            <Box className={styles.buttonWrapper}>
+      {data && (
+        <>
+          <Box className={styles.contentWrapper}>
+              <Box className={styles.buttonWrapper}>
                 <Button type="button" variant="outlined" onClick={() => setModalOpen(true)}>
-                    내역 추가
+                  내역 추가{data.id}
                 </Button>
-            </Box>
-            <Box className={styles.gridWrapper}>
+              </Box>
+              <Box className={styles.gridWrapper}>
                 <HistoryGrid />
+              </Box>
             </Box>
-        </Box>
-
-      <HistoryAddModal open={modalOpen} handleClose={() => setModalOpen(false)} />
+          <HistoryAddModal open={modalOpen} handleClose={() => setModalOpen(false)} />
+        </>
+      )}
     </DefaultLayout>
   );
 }
