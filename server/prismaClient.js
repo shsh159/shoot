71418f2e2005b4a45edb32dcb3bd5f5316a9e4ce
shsh159 <sunghoon8467@gmail.com>
2025-04-21 18:03:57 +0900

    prisma 주석 추가

--- server/prismaClient.js
+++ server/prismaClient.js
@@ -1,3 +1,6 @@
+// db 스키마 변경되거나 schema.prisma 수정하면 실행
+// npx prisma db pull
+// npx prisma generate
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 module.exports = prisma;
