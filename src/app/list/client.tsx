ccc12af9fe0c02b7d28f7fbec3c28417093a52f4
shsh159 <sunghoon8467@gmail.com>
2025-05-02 14:11:38 +0900

    client 컴포넌트 분리

---
+++ src/app/list/client.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { useGetHistoryList } from '@api/history.quries';
+import { Box, Button, Skeleton } from '@mui/material';
+import { useState } from 'react';
+import styles from './client.module.scss';
+import HistoryGrid from '@components/grid/HistoryGrid';
+import HistoryAddModal from '@components/modal/HistoryAddModal';
+
+export default function Client() {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+  const { data: historyList, isLoading } = useGetHistoryList();
+  return (
+    <>
+      {!isLoading ? (
+        <>
+          <Box className={styles.contentWrapper}>
+            <Box className={styles.buttonWrapper}>
+              <Button
+                type="button"
+                variant="outlined"
+                onClick={() => setModalOpen(true)}
+              >
+                내역 추가
+              </Button>
+            </Box>
+            <Box className={styles.gridWrapper}>
+              <HistoryGrid historyList={historyList} />
+            </Box>
+          </Box>
+          <HistoryAddModal
+            open={modalOpen}
+            handleClose={() => setModalOpen(false)}
+          />
+        </>
+      ) : (
+        <Box className={styles.contentWrapper}>
+          <Box className={styles.buttonWrapper}>
+            <Skeleton
+              variant="rounded"
+              width={88}
+              height={35}
+              animation="wave"
+            />
+          </Box>
+
+          <Box className={styles.gridWrapper}>
+            <Skeleton
+              variant="rounded"
+              animation="wave"
+              width="100%"
+              height="100%"
+              className={styles.gridWrapper}
+            />
+          </Box>
+        </Box>
+      )}
+    </>
+  );
+}
