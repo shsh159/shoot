35a38645fa58d76a09763ed9dff36efcdbf458c9
shsh159 <sunghoon8467@gmail.com>
2025-05-19 16:48:48 +0900

    대시보드 스켈레톤 적용

--- src/app/dashboard/client.tsx
+++ src/app/dashboard/client.tsx
@@ -2,7 +2,7 @@
 
 import { useGetHistoryMonth, useGetHistoryYear } from '@api/history.quries';
 import CardComponent from '@components/card/Card';
-import { Box } from '@mui/material';
+import { Box, Skeleton } from '@mui/material';
 import dayjs from 'dayjs';
 import dynamic from 'next/dynamic';
 
@@ -15,8 +15,9 @@ const YearlyChart = dynamic(() => import('@components/chart/YearlyChart'), {
 
 export default function Client() {
   const currentMonth = dayjs().format('YYYY-MM');
-  const { data: monthData } = useGetHistoryMonth(currentMonth);
-  const { data: yearData } = useGetHistoryYear();
+  const { data: monthData, isLoading: isMonthLoading } =
+    useGetHistoryMonth(currentMonth);
+  const { data: yearData, isLoading: isYearLoading } = useGetHistoryYear();
 
   return (
     <>
@@ -25,14 +26,46 @@ export default function Client() {
         gap={2}
         sx={{ height: '380px', marginBottom: '20px' }}
       >
-        {monthData && <MonthlyChart amountList={monthData?.amountList} />}
-        {monthData?.totalAmount && (
-          <CardComponent totalAmount={monthData.totalAmount} />
-        )}
+        <Box sx={{ width: '70%' }}>
+          {monthData?.amountList ? (
+            <MonthlyChart amountList={monthData.amountList} />
+          ) : (
+            <Skeleton
+              variant="rounded"
+              animation="wave"
+              width="100%"
+              height="100%"
+            />
+          )}
+        </Box>
+        <Box sx={{ width: '20%' }}>
+          {monthData?.totalAmount ? (
+            <CardComponent totalAmount={monthData.totalAmount} />
+          ) : (
+            <Skeleton
+              variant="rounded"
+              animation="wave"
+              width="100%"
+              height="100%"
+            />
+          )}
+        </Box>
       </Box>
-      <Box sx={{ height: '380px' }}>
-        {yearData && <YearlyChart amountList={yearData?.amountList} />}
-      </Box>
+
+      {!isYearLoading ? (
+        <Box sx={{ height: '380px' }}>
+          <YearlyChart amountList={yearData?.amountList ?? []} />
+        </Box>
+      ) : (
+        <Box sx={{ height: '380px' }}>
+          <Skeleton
+            variant="rounded"
+            animation="wave"
+            width="100%"
+            height="100%"
+          />
+        </Box>
+      )}
     </>
   );
 }
