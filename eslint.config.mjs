f5ed80c7ad041fad777412ddf8502542622d6af8
shsh159 <sunghoon8467@gmail.com>
2025-06-18 14:36:26 +0900

    eslint rule 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default [
   ...compat.extends('next/core-web-vitals', 'plugin:prettier/recommended'),
 
   {
-    files: ['**/*.{ts,tsx,js,jsx}'],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       parserOptions: {
         ecmaVersion: 2022,
@@ -26,6 +26,7 @@ export default [
       'linebreak-style': ['error', 'unix'], // 줄바꿈 LF (유닉스)
       quotes: ['error', 'single'], // 작은 따옴표 사용
       'prettier/prettier': 'error', // prettier와 충돌 방지
+      eqeqeq: ['error', 'always'],
     },
   },
 ];
