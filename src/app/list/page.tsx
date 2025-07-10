64325378a3d6f82ac0f1b555ab48fe398b480941
shsh159 <sunghoon8467@gmail.com>
2025-07-10 10:00:04 +0900

    metadata title 적용

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,5 +1,10 @@
 import DefaultLayout from '@components/layout/DefaultLayout';
 import Client from './client';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'List',
+};
 
 export default function Page() {
   return (
